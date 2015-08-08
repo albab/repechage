652918b6b11f739feb6f9910e1fc6d615f23dd50
Alex <ababich@asu.edu>
2015-08-07 22:06:07 -0700

    Started adding history of tweets and users in JSON by serializing the hash and loading it back in, think it works for now

--- main.rb
+++ main.rb
@@ -1,14 +1,9 @@
 require 'twitter'
+require 'json'
 
 @follow_and_retweet = Hash.new(0)
-number_of_contests_to_enter = 500
-
-# streaming_client = Twitter::Streaming::Client.new do |config|
-#   config.consumer_key       = 'ZYztcGvW08hh513W354Ix2MVu'
-#   config.consumer_secret    = 'ZvV0sGsCjN8c0n2roZJPK7krjsJ6dpvdvOo8xHQIc1iXZz3iy6'
-#   config.access_token        = '3307168590-cYjuX5LQqnzOBp9eIfYkcCG3sMBTqp04UgOxWlo'
-#   config.access_token_secret = 'JtA75M9d5KFdlmELadPzLV33K3Ekg861DyAEw7fotdpCu'
-# end
+@follow_and_retweet_history = JSON.parse(File.read('data/users_and_retweets.json'))
+number_of_contests_to_enter = 10
 
 rest_client = Twitter::REST::Client.new do |config|
   config.consumer_key       = 'ZYztcGvW08hh513W354Ix2MVu'
@@ -19,10 +14,9 @@ end
 
 enter_to_win = "RT follow enter win"
 rest_client.search(enter_to_win, result_type: "recent").take(number_of_contests_to_enter).collect do |object|
-# streaming_client.filter(track: enter_to_win, result_type: "recent") do |object|
 	count = 0
   if object.retweeted_status && object.is_a?(Twitter::Tweet)
-  	if @follow_and_retweet.has_key? object.retweeted_status.user.id.to_i
+  	if @follow_and_retweet.has_key? object.retweeted_status.user.id.to_i or @follow_and_retweet_history.has_key? object.retweeted_status.user.id.to_i
   		puts "Already added #{object.retweeted_status.user.id.to_i} to list...\n\n"
   	else
   		@follow_and_retweet.merge! object.retweeted_status.user.id.to_i => object.retweeted_status.id.to_i
@@ -50,13 +44,27 @@ puts "/////////////////////////////////////////////////////////////////////////"
 		rest_client.follow user
 		puts "Followed user: #{user}"
     sleep(1)
-		rest_client.retweet tweet
-		puts "Retweeted tweet: #{tweet}"
+		# rest_client.retweet tweet
+		# puts "Retweeted tweet: #{tweet}"
 	rescue => error
     puts error
 		next
 	end
 end
+
+puts "\nBacking up...\n"
+
+begin
+  file = File.write("data/users_and_retweets.json", @follow_and_retweet.to_json)
+  file = File.write("data/users_and_retweets.json", @follow_and_retweet_history.to_json)
+rescue IOError => e
+  #some error occur, dir not writable etc.
+ensure
+  # file.close unless file.nil?
+end
+
+
+
 
 puts "\n~~~~\n"
 puts "Fini."
